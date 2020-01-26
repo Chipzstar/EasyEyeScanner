306daa7aa42901e494306659c6587431eff20185
Chisom Oguibe <chipzstar.dev@googlemail.com>
2020-01-26 15:33:05 +0000

    - add landing page UI
    - card custom component to display each scanned document
    - flatlist style layout for card tiles

--- app/screens/ScansScreen/styles.js
+++ app/screens/ScansScreen/styles.js
@@ -6,7 +6,7 @@ const {height: HEIGHT} = Dimensions.get("window"); //Max Height of phone screen
 export default (styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
+		paddingTop: 40,
+		paddingHorizontal: 20,
+	},
 }));
