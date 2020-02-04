f6bb47452d196c2ce4bebfd7498665ed4cfd59a6
Chisom Oguibe <chipzstar.dev@gmail.com>
2020-02-04 14:30:04 +0000

    add image editing functionality - crop - replace - flip - rotate
    Signed-off-by: Chisom Oguibe <chipzstar.dev@gmail.com>

--- app/components/Camera/ImagePreview.js
+++ app/components/Camera/ImagePreview.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import {Image, Text} from 'react-native';
-import {Container, Content, Header, Title, Left, Body, Icon, Right, Footer, FooterTab, Button} from 'native-base'
-import {MaterialIcons, MaterialCommunityIcons} from '@expo/vector-icons';
+import {Body, Button, Container, Content, Footer, FooterTab, Header, Icon, Left, Right, Title} from 'native-base'
+import {MaterialCommunityIcons, MaterialIcons} from '@expo/vector-icons';
 import {connect} from 'react-redux';
-import {removePhoto} from "../../../store/actions/capturesAction";
+import {removePhoto, replacePhoto} from "../../../store/actions/capturesAction";
+import * as ImagePicker from 'expo-image-picker';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 class ImagePreview extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			accept: true,
-			discard: false
+			photoData: Object.assign(this.props.navigation.state.params)
 		};
 	}
 
@@ -19,8 +20,44 @@ class ImagePreview extends Component {
 		this.props.navigation.goBack();
 	};
 
+	replacePhoto = async (uri) => {
+		const options = {
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: false,
+			quality: 1
+		};
+		let photo = await ImagePicker.launchImageLibraryAsync(options);
+
+		if (!photo.cancelled) {
+			this.props.replacePhoto(uri, photo);
+			this.props.navigation.goBack();
+		}
+	};
+
+	rotate90 = async (uri) => {
+		let result = await ImageManipulator.manipulateAsync(
+			uri,
+			[{rotate: 90}],
+			{compress: 1, format: ImageManipulator.SaveFormat.PNG}
+		);
+		console.log(result);
+		this.props.replacePhoto(uri, result);
+		this.setState({photoData: result});
+	};
+
+	flipPhoto = async (uri, width, height) => {
+		let result = await ImageManipulator.manipulateAsync(
+			uri,
+			[{flip: height > width ? ImageManipulator.FlipType.Horizontal : ImageManipulator.FlipType.Vertical}],
+			{compress: 1, format: ImageManipulator.SaveFormat.PNG}
+		);
+		console.log('Flipped photo:', result);
+		this.props.replacePhoto(uri, result);
+		this.setState({photoData: result});
+	};
+
 	render() {
-		const photoData = Object.assign(this.props.navigation.state.params);
+		let {photoData} = this.state;
 		let key = this.props.captures.findIndex(photo => photo.uri === photoData.uri) + 1;
 		console.log("key: ", key);
 		//const ratio = photoData.height / photoData.width;
@@ -40,24 +77,25 @@ class ImagePreview extends Component {
 						<Text style={{fontSize: 20, color: 'white', fontWeight: 'bold'}}>Confirm</Text>
 					</Button>*/}
 				</Header>
-				<Content>
+				<Content scrollEnabled>
 					<Image source={{uri: photoData.uri}}
 					       style={{flex: 1, justifyContent: 'center', alignItems: 'center', aspectRatio: 0.5}}
 					       resizeMode="contain"/>
 				</Content>
 				<Footer>
 					<FooterTab>
-						<Button vertical>
+						<Button vertical onPress={() => this.replacePhoto(photoData.uri)}>
 							<MaterialCommunityIcons name={"file-replace"} size={28} color={'white'}/>
 							<Text style={styles.iconText}>Replace</Text>
 						</Button>
-						<Button vertical>
+						<Button vertical onPress={() => this.rotate90(photoData.uri)}>
 							<MaterialIcons name={'rotate-right'} size={28} color={'white'}/>
 							<Text style={styles.iconText}>Rotate</Text>
 						</Button>
-						<Button vertical active>
-							<Icon name="crop"/>
-							<Text style={styles.iconText}>Crop</Text>
+						<Button vertical
+						        onPress={() => this.flipPhoto(photoData.uri, photoData.width, photoData.height)}>
+							<MaterialIcons name={"flip"} color={'white'} size={28}/>
+							<Text style={styles.iconText}>Flip</Text>
 						</Button>
 						<Button vertical onPress={() => this.deletePhoto(photoData.uri)}>
 							<MaterialIcons name={'delete'} size={28} color={'white'}/>
@@ -80,6 +118,9 @@ const mapDispatchToProps = (dispatch) => {
 	return {
 		removePhoto: (uri) => {
 			dispatch(removePhoto(uri))
+		},
+		replacePhoto: (oldURI, newPhoto) => {
+			dispatch(replacePhoto(oldURI, newPhoto))
 		}
 	}
 };
--- store/actions/capturesAction.js
+++ store/actions/capturesAction.js
@@ -1,5 +1,6 @@
 export const ADD_PHOTO = 'ADD_PHOTO';
 export const REMOVE_PHOTO = 'REMOVE_PHOTO';
+export const REPLACE_PHOTO = 'REPLACE_PHOTO';
 
 export const addPhoto = (photo) => {
 	return {
@@ -12,6 +13,14 @@ export const removePhoto = (uri) => {
 	return {
 		type: REMOVE_PHOTO,
 		photoURI: uri
+	}
+};
+
+export const replacePhoto = (oldURI, newPhoto) => {
+	return {
+		type: REPLACE_PHOTO,
+		oldPhotoURI: oldURI,
+		newPhoto: newPhoto
 	}
 };
 
