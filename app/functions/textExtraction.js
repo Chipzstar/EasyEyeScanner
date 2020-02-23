59dc6a0a7e80cce349f6cfbbf64b2d55d73ecd12
Chisom Oguibe <chipzstar.dev@gmail.com>
2020-02-23 11:06:58 +0000

    - api integration

--- app/functions/textExtraction.js
+++ app/functions/textExtraction.js
@@ -1,26 +1,21 @@
-import Predictions from '@aws-amplify/predictions';
-import {Storage} from 'aws-amplify';
+import {API} from 'aws-amplify';
 import React from 'react';
+import awsmobile from "../../aws-exports";
 
 export default function TextIdentification() {
 	console.log('Function entered...');
-	const testIdentityID = "eu-west-2:1bbd817b-6d01-4f56-b903-0ea8a7598256";
-	const testKey = '1ayTEF.jpg';
-	Storage.get(testKey, {download: false, identityId: testIdentityID})
-		.then(file => {
-			console.log(`File obtained: ${file}`);
-			Predictions.identify({
-				text: {
-					source: {
-						identityId: testIdentityID,
-						key: testKey,
-						level: 'public'
-					},
-					format: "PLAIN", // Available options "PLAIN", "FORM", "TABLE", "ALL"
-				}
-			}).then(({text: {fullText}}) => {
-				console.log(`\nFULL TEXT:\n\n${fullText}`);
-			}).catch(err => console.error(JSON.stringify(err, null, 2)))
-		})
-		.catch(err => console.log(err));
+	let apiName = awsmobile.aws_cloud_logic_custom[0].name;
+	let path = '/scan/getText';
+	let myInit = { // OPTIONAL
+		body: {
+			message: "Testing..."
+		}, // replace this with attributes you need
+		headers: {
+			'Content-Type': "application/json",
+			'Accept': "application/json"
+		},
+		response: true// OPTIONAL
+	};
+	console.log('Making Api request...');
+	API.post(apiName, path, myInit).then(res => console.log(res.data)).catch(err => console.error(err));
 };
